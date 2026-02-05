1a104eb51ff97517d616cc3f7076f4871c431523
Tiago Matos <tiagaomatos@hotmail.com>
2026-02-05 21:33:49 +0000

    created library to make rooms

--- src/main.c
+++ src/main.c
@@ -65,31 +65,7 @@ void map_generator()
 		}
 		printf("\n");
 	}
-
 }
-
-void room_generator()
-{
-	for(int room_row = 0; room_row < ROOM_SIZE_X; room_row++)
-	{
-		for(int room_column = 0; room_column < ROOM_SIZE_Y; room_column++)
-		{
-			if( room_row == 0 || room_row == ROOM_SIZE_X - 1)
-			{
-				room[room_row][room_column] = '#';
-			}
-			else if(room_column == 0 || room_column == ROOM_SIZE_Y - 1)
-			{
-				room[room_row][room_column] = '#';
-			}
-			else
-			{
-				room[room_row][room_column] = ' ';
-			}
-		}
-	}
-}
-
 
 void draw_playfield()
 {
