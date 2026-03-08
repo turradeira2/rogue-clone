ae1f080f084ff61519b92788ac646dbf6b89fe03
Tiago Matos <tiagaomatos@hotmail.com>
2026-03-08 01:07:30 +0000

    test maps

---
+++ include/test_maps.h
@@ -0,0 +1,9 @@
+#ifndef TEST_MAPS_H
+#define TEST_MAPS_H
+
+#define MAP_X 5
+#define MAP_Y 5
+
+extern int map[MAP_X][MAP_Y];
+
+#endif
---
+++ src/test_maps.c
@@ -0,0 +1,17 @@
+//#include <stdlib.h>
+
+#include "../include/test_maps.h"
+//#include "../include/rooms.h"
+//#include "../include/game_objects.h"
+
+void test_map_1()
+{
+    int map[MAP_X][MAP_Y]={{0,1,1,0,0},{0,1,1,1,0},{0,0,1,1,0},{0,1,1,1,0},{0,1,1,1,0}};
+    /*
+     * {0,1,1,0,0},
+     * {0,1,1,1,0},
+     * {0,0,1,1,0},
+     * {0,1,1,1,0},
+     * {0,1,1,1,0}
+     */
+}
